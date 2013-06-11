1d62066f19e75cfb1d56d2eced9ba29370325fe4
User <User@a204.aulas.local>
2013-06-11 10:59:42 -0700

    agregado el controller que vamos a utilizar

---
+++ ejercicio2/ejercicio2/ESNoticiasViewController.h
@@ -0,0 +1,13 @@
+//
+//  ESNoticiasViewController.h
+//  ejercicio2
+//
+//  Created by User on 6/11/13.
+//  Copyright (c) 2013 IGG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESNoticiasViewController : UIViewController
+
+@end
---
+++ ejercicio2/ejercicio2/ESNoticiasViewController.m
@@ -0,0 +1,38 @@
+//
+//  ESNoticiasViewController.m
+//  ejercicio2
+//
+//  Created by User on 6/11/13.
+//  Copyright (c) 2013 IGG. All rights reserved.
+//
+
+#import "ESNoticiasViewController.h"
+
+@interface ESNoticiasViewController ()
+
+@end
+
+@implementation ESNoticiasViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
